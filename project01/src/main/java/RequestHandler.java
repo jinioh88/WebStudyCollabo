85198a458e6bb913d5f6bc4d8f8246f5d596b255
ohsejin <jinioh88@gmail.com>
2018-10-13 20:45:13 +0900

    요구사항2 - 책버전

--- project01/src/main/java/RequestHandler.java
+++ project01/src/main/java/RequestHandler.java
@@ -1,9 +1,12 @@
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -34,10 +37,19 @@ public class RequestHandler extends Thread {
                 log.debug("header : {}",line);
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+tokens[1]).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos,body);
+            String url = tokens[1];
+            if("/user/create".startsWith("url")) {
+                int index = url.indexOf("?");
+                String queryString = url.substring(index+1);
+                Map<String,String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User : {}",user);
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp"+tokens[1]).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos,body);
+            }
         } catch(IOException e) {
             log.error(e.getMessage());
         }
---
+++ project01/src/main/java/model/User.java
@@ -0,0 +1,29 @@
+package model;
+
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
