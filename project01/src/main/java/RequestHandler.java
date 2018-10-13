628e8cff9329a45cdf1b9e2ad27860aa61edbd74
ohsejin <jinioh88@gmail.com>
2018-10-13 20:17:19 +0900

    요구사항 1 - 책버전

---
+++ project01/src/main/java/RequestHandler.java
@@ -0,0 +1,66 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+
+public class RequestHandler extends Thread {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private Socket connection;
+
+    public RequestHandler(Socket connecionSocket) {
+        this.connection = connecionSocket;
+    }
+
+    @Override
+    public void run() {
+        log.debug("New Client Connect! Connected IP : {}, Port : {}",connection.getInetAddress(), connection.getPort());
+
+        try(InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}",line);
+
+            if(line==null) {
+                return;
+            }
+
+            String[] tokens = line.split(" ");
+
+            while(!line.equals("")) {
+                line = br.readLine();
+                log.debug("header : {}",line);
+            }
+
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = Files.readAllBytes(new File("./webapp"+tokens[1]).toPath());
+            response200Header(dos, body.length);
+            responseBody(dos,body);
+        } catch(IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthBodyContent) {
+        try {
+            dos.writeBytes("HTTP:/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: "+lengthBodyContent+"\r\n");
+            dos.writeBytes("\r\n");
+        } catch(IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch(IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
